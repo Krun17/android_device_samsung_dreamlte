2ee6e68fa3ef313414e278a9eda7a95cdfae77ec
Gayos24 <remy.zulauff@gmail.com>
2018-02-09 16:13:53 +0100

    dreamlte: i will not even comment this ...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/universal8895
 TARGET_KERNEL_CONFIG := exynos8895-dreamlte_eur_open_defconfig
 
 # Recovery
-TARGET_OTA_ASSERT_DEVICE := dream2lte
+TARGET_OTA_ASSERT_DEVICE := dreamlte
 
 # Inherit common board flags
 include device/samsung/dream-common/BoardConfigCommon.mk
